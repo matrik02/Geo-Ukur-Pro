e562861e150f37b1e13c1e07398bead6611e8e85
matrik02 <166111353+matrik02@users.noreply.github.com>
2026-02-27 12:58:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
@@ -66,6 +66,7 @@ else:
         st.session_state['login_berjaya'] = False
 
         st.rerun()
+
 
 
 
